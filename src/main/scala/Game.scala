e2f09ced2eb7d1ce9509c41ee3ff2a373c91a12d
Terrance Niechciol <TNiechciol@gmail.com>
2013-11-12 01:42:20 -0500

    Made updating and drawing happen at different rates

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -12,53 +12,100 @@ import scala.collection.mutable.Stack
 
 object Game extends App {
   Console.println(System.getProperty("java.library.path"))
-  val fpsCap = 180
+  val updateFPSCap = 500
+  val drawFPSCap = 80
   var gameStates = new Stack[GameState]
-  var lastFrameTime:Long = 0
   gameStates.push(new GS_Init)
-  gameStates.head.init
 
-  var fpsCountStart:Long = System.nanoTime
-  var fpsFrameCount:Int = 0
+  var threadsExit = false
+  var shouldDraw = true
 
-  while (!Display.isCloseRequested && !gameStates.isEmpty) {
-    val startTime = System.nanoTime
-    val currentState = gameStates.head
+  class GameRunner extends Thread {
+    override def run() = {
+      gameStates.synchronized {
+        // the first thread will make an opengl context, so it needs to be initialized here
+        gameStates.head.init
+      }
+      var exit = false
+      var lastFrameTime:Long = 0
+      var fpsCountStart:Long = System.nanoTime
+      var fpsFrameCount:Int = 0
+      while (!exit) {
+        var startTime:Long = 0
+        gameStates.synchronized {
+          threadsExit = Display.isCloseRequested && !gameStates.isEmpty
+          exit = threadsExit
 
-    // update the current state
-    currentState.update(lastFrameTime / 1000000000d)
+          startTime = System.nanoTime
+          val currentState = gameStates.head
+          // update the current state
+          currentState.update(lastFrameTime / 1000000000d)
 
-    // if that update caused the state to die, remove to from the stack
-    // otherwise, draw the state
-    if (!currentState.isAlive) {
-      gameStates.pop
-    } else {
-      currentState.draw
+          // if that update caused the state to die, remove to from the stack
+          // otherwise, draw the state
+          if (!currentState.isAlive) {
+            gameStates.pop
+          }
+
+          // since updating includes opengl calls and runs much faster, draw is run from here
+          if (shouldDraw) {
+            currentState.draw
+            Display.update
+            shouldDraw = false
+          }
+
+          // check if there is a next state
+          if (currentState.hasNextState) {
+            gameStates.push(currentState.takeNextState)
+            gameStates.head.init
+          }
+        } // nothing else depends on shared data, so exit the synchronized section
+
+        // calculate the time taken and delay the game if enabled
+        val endTime = System.nanoTime
+        val delayTime = (1000d / updateFPSCap.toDouble) - ((endTime - startTime) / 1000000)
+        if (delayTime > 0) Thread.sleep(delayTime.toInt)
+        fpsFrameCount += 1
+        val fpsTimeDiff = System.nanoTime - fpsCountStart
+        if (fpsTimeDiff >= 1000000000) {
+          Console.println("fps: " + (1000000000d / (fpsTimeDiff / fpsFrameCount)))
+          fpsFrameCount = 0
+          fpsCountStart = System.nanoTime
+        }
+        lastFrameTime = System.nanoTime - startTime
+      }
+      Display.destroy()
     }
-
-    // update window, handle events, etc
-    Display.update
-
-    // check if there is a next state
-    if (currentState.hasNextState) {
-      gameStates.push(currentState.takeNextState)
-      gameStates.head.init
-    }
-
-    // calculate the time taken and delay the game accordingly
-    val endTime = System.nanoTime
-    val delayTime = (1000d / fpsCap.toDouble) - ((endTime - startTime) / 1000000)
-    if (delayTime > 0) Thread.sleep(delayTime.toInt)
-    fpsFrameCount += 1
-    val fpsTimeDiff = System.nanoTime - fpsCountStart
-    if (fpsTimeDiff >= 1000000000) {
-      Console.println("fps: " + (1000000000d / (fpsTimeDiff / fpsFrameCount)))
-      fpsFrameCount = 0
-      fpsCountStart = System.nanoTime
-    }
-    lastFrameTime = System.nanoTime - startTime
   }
 
-  Display.destroy()
+  class DrawScheduler extends Thread {
+    override def run() {
+      var exit = false
+      var fpsCountStart:Long = System.nanoTime
+      var fpsFrameCount:Int = 0
+      while (!exit) {
+        val startTime = System.nanoTime
+        gameStates.synchronized {
+          exit = threadsExit
+
+          shouldDraw = true
+        } // nothing else depends on shared data, so exit the synchronized section
+        val endTime = System.nanoTime
+        val delayTime = (1000d / drawFPSCap.toDouble) - ((endTime - startTime) / 1000000)
+        if (delayTime > 0) Thread.sleep(delayTime.toInt)
+        fpsFrameCount += 1
+        val fpsTimeDiff = System.nanoTime - fpsCountStart
+        if (fpsTimeDiff >= 1000000000) {
+          Console.println("fps: " + (1000000000d / (fpsTimeDiff / fpsFrameCount)))
+          fpsFrameCount = 0
+          fpsCountStart = System.nanoTime
+        }
+      }
+    }
+  }
+
+  new Thread(new GameRunner()).start
+  new DrawScheduler().run // call act here to it must finish before the future finishes
+
   sys.exit(0)
 }
